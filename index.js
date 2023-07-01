340e53f99f4f2dd553ae646965ee69d58ab485bf
MADERU-CM <cliv.moyia@student.moringaschool.com>
2023-07-01 17:53:17 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+let customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value';
+}
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer,
+};
+
